91beb3b717a7af3c34145266f76409b40f7ec340
Vern McGeorge <odnt@VernMcGeorge.com>
2011-01-14 17:05:16 -0800

    added a pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 HartlSample1::Application.routes.draw do
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +50,10 @@ HartlSample1::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  
+  root :to => "pages#home"
+  get "pages/home"
+  get "pages/contact"
 
   # See how all your routes lay out with "rake routes"
 
