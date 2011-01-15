e6650efa06efdedff51694ec9f61311395b9af22
Vern McGeorge <odnt@VernMcGeorge.com>
2011-01-14 16:32:55 -0800

    Rails Tutorial Sample App Initial Install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HartlSample1::Application.config.secret_token = 'abd8d60f7047c2673f9e199abdf17d996d3b280dc979a7bea680fb1756f3324bc5059d625d36ae061087608ecf3a01c1ba917303649336bd5b57dc2e6bdde98f'
