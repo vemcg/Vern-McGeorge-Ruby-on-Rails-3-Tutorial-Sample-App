7d3917b0ef7aee9a718db440a9eb2c568c556c84
Vern McGeorge <odnt@VernMcGeorge.com>
2011-01-20 15:54:54 -0800

    Finished first cut of User Model - Chap 5

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'sqlite3-ruby', :require => 'sqlite3'
 # end
 group :test, :development do
 gem 'rspec-rails', '2.0.0.beta.18'
+gem 'annotate-models', '1.0.4'
 # gem 'capybara'
 gem 'webrat', '0.7.1'
 gem 'database_cleaner'
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ HartlSample1::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  
+  resources :users
+  match '/signup', :to => 'users#new'
 
   # Sample resource route with options:
   #   resources :products do
